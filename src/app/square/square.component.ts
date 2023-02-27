970437c48e9196f62518468b87cb25f5136c583c
Skrish02 <sskrish786@gmail.com>
2023-02-27 17:26:09 +0530

    Commit

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { GameService } from '../game.service';
+
 
 @Component({
   selector: 'app-square',
   templateUrl: './square.component.html',
   styleUrls: ['./square.component.css']
 })
-export class SquareComponent {
+export class SquareComponent implements OnInit {
 
+  @Input() square: any;
+
+  constructor( public gameService: GameService) {}
+
+  ngOnInit() {
+
+  }
+
+  changePlayer() {
+
+    this.gameService.isGameRunning = true;
+
+    if( this.gameService.isGameRunning && this.square.state === null) {
+      this.square.state = this.gameService.activePlayer;
+      this.gameService.changePlayerTurn(this.square);
+    }
+  }
 }
+  
